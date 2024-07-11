cc772a4b836c6c86c842a0c02a227df2369283e6
Tiancheng Mai <58879993+mtc2000@users.noreply.github.com>
2024-07-11 12:43:04 +1000

    add: log analysis scripts

---
+++ anaylsis.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+experiment_folder=$(pwd)/$1
+
+if [ ! -d "$experiment_folder" ]; then
+   echo $experiment_folder is not a dir
+   echo plz check again
+   exit 1
+fi
+
+echo analysizing this folder: $experiment_folder
+cd $experiment_folder
+
+echo
+echo configuration:
+find $experiment_folder -type f -name "configuration.txt" | xargs cat
+echo
+
+echo "worst adjusted_running_time and its breakdown by multicast_time and computation_time (in miliseconds)"
+
+for repeat in $(find $experiment_folder -type d -name "Repeat*");
+do
+   for log_file in $(find $repeat -type f -name "*.log");
+   do
+      # echo $log_file
+      if [ $(grep -ic 'broadcastBlockchainRelay' "$log_file") -ge 1 ]; then
+         continue
+      fi
+
+      multicast_send_time=$(grep -A1 '^time to send multicast complaint$' "$log_file" | tail -n1)
+      multicast_receive_time=$(grep -A1 '^time to receive multicast complaint$' "$log_file" | tail -n1)
+      multicast_time=$(($multicast_send_time + $multicast_receive_time))
+      computation_time=$(grep -A1 '^total computation cost:$' "$log_file" | tail -n1)
+      adjusted_running_time=$(($multicast_time + $computation_time))
+      echo $adjusted_running_time,$multicast_time,$computation_time
+   done | sort -t',' -k 1,1nr | head -n1
+done | sort -t',' -k 1,1nr | head -n1
+
+echo "(worst) of worst bandwidth usage data (in bytes)"
+
+for repeat in $(find $experiment_folder -type d -name "Repeat*");
+do
+   for log_file in $(find $repeat -type f -name "*.log");
+   do
+      # echo $log_file
+      if [ $(grep -ic 'broadcastBlockchainRelay' "$log_file") -ge 1 ]; then
+         continue
+      fi
+
+      grep -A1 '^total data:$' "$log_file" | tail -n1
+   done
+done | datamash -W --output-delimiter=',' max 1 # mean 1 # uncomment this if you wish to see the (mean) of the worst bandwidth over repeats
+
+echo
---
+++ anaylsize_results_expected.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# this script takes ~30 seconds to run
+
+echo "Fig. 3: End-to-end Test Results (a) and the red line (bad) in (b) come from the analysis of the following raw logs"
+find results_expected/ -maxdepth 1 -mindepth 1 | sort -t '-' -k1,1V | grep corrupted
+
+# batch analysis
+find results_expected/ -maxdepth 1 -mindepth 1 | sort -t '-' -k1,1V | grep corrupted | xargs -I{} bash anaylsis.sh {}
+
+# one-by-one
+# bash anaylsis.sh results_expected/GroupSize17-ExpIdcorrupted-0428-atSize38
+# bash anaylsis.sh results_expected/GroupSize33-ExpIdcorrupted-0428-atSize38
+# bash anaylsis.sh results_expected/GroupSize65-ExpIdcorrupted-0428-atSize38
+# bash anaylsis.sh results_expected/GroupSize129-ExpIdcorrupted-0428-atSize38
+# bash anaylsis.sh results_expected/GroupSize257-ExpIdcorrupted-0428-atSize38
+
+echo "The blue line (good) in Fig. 3: End-to-end Test Results (b) comes from the analysis of the following raw logs"
+find results_expected/ -maxdepth 1 -mindepth 1 | sort -t '-' -k1,1V | grep normal
+
+# batch analysis
+find results_expected/ -maxdepth 1 -mindepth 1 | sort -t '-' -k1,1V | grep normal | xargs -I{} bash anaylsis_bandwidth.sh {}
+
+# one-by-one
+# bash anaylsis_bandwidth.sh results_expected/GroupSize17-ExpIdnormal-0428-atSize38
+# bash anaylsis_bandwidth.sh results_expected/GroupSize33-ExpIdnormal-0428-atSize38
+# bash anaylsis_bandwidth.sh results_expected/GroupSize65-ExpIdnormal-0428-atSize38
+# bash anaylsis_bandwidth.sh results_expected/GroupSize129-ExpIdnormal-0428-atSize38
+# bash anaylsis_bandwidth.sh results_expected/GroupSize257-ExpIdnormal-0428-atSize38
